cb6d572020b212dac84b209419118c9652c885df
Slava Circel <slava@canva.com>
2022-07-09 23:25:33 +1000

    Added URL params for path, subtitles and mediaProfile; adding initial offloading function, in order to move the downloaded files to an external USB drive; moved archive txt files outside of the download folder; added YouTube channel URL auto-detection; improved logging; added logs per run and per channel to quickly identify the broken or outdated channels; added a proper main function; general refactoring;

--- README.md
+++ README.md
@@ -13,54 +13,76 @@ Don't need to install anything, just download the repository and extract the fil
 
 	1.2A Using Pip
 
-		pip install -r requirements.txt 
+		pip install -r requirements.txt or
 
 	1.2B
+		python -m pip install --upgrade yt-dlp or
 
-		python -m pip install --upgrade yt-dlp
+	1.2C 
+		Use the one from the /bin folder, just add it to your PATH
 
 
 1.3 Verify it has been installed properly, run the following command: yt-dlp --version
 
-Should return something like:  2022.02.04
+Should return something like:  2022.05.18
 
 
 # 2 Setup folder structure for your channels
+
+
+A. YT Library folder structure: 
+
 The script is reading the channel ID from the second string of the folder separated by space.
 Currently there are 4 types of YT URLs supported: (Let me know if you find more)
 
+Standard channels Examples, i.e.: 
 a) https://www.youtube.com/user/_username_/videos
-	
-b) https://www.youtube.com/c/'_channel-name_'/videos
+   https://www.youtube.com/user/partnersupport
 
+b) https://www.youtube.com/c/'_channel-name_'/videos
+   https://www.youtube.com/c/YouTubeCreators
+   https://www.youtube.com/YouTubeCreators
+
+or Legacy Channels Examples, just use the long channel ID, and the script will recognize it - i.e.:
 c) https://www.youtube.com/channel/'_channel-hash-ID_'/videos
-	
-d) https://www.youtube.com/channel/'_channel-hash-ID_'
+   https://www.youtube.com/channel/'_channel-hash-ID_'
+   https://www.youtube.com/channel/UCUZHFZ9jIKrLroW8LcyJEQQ 
+
+<RandomChosenName YouTubeCreators> <- folder with videos
+RandomChosenName_(YouTubeCreators).txt <- text archive with already downloaded vids, no spaces, no special characters
 
 For the first two each folder must contain two string: AnyGivenName username or AnyGivenName channel-name
 
-	For example: FDF FamilyDrivenFaith 
+	For example: FDF (FamilyDrivenFaith) 
 
-For the later two, make sure to include LgcyChn as part of the first string: AnyGivenName HashID
+For the later two, AnyGivenName HashID
 
-	For example:  AO&OProductions_LgcyChn UCz4Qb-1lIttNRgB_lQDm6eA
+	For example:  AOProductions (UCz4Qb-1lIttNRgB_lQDm6eA)
 
+It will automatically create FDF_(FamilyDrivenFaith).txt and AOProductions_(UCz4Qb-1lIttNRgB_lQDm6eA).txt where all the already downloaded filenames will be stored.
+
+
+B. Helper utilities
+You can offload the videos later to an external hdd, and continue downloading only 
+the new uploads using the txt archives
 
 
 # 3 run the script 
-A. Binary (easy) - make sure you add / update all your channels in the "data" folder
-Run from terminal: 
-
-Open bin/ folder
-
-aggregator.exe test/
-
-B. From Source
-Run the script using "python aggregator.py \Path\to\your\channels\folder\"
-
-
+A. From Source
+Run the script using python aggregator.py -path "<Full path to download dir>" -sub "Subs,separated,by,comma" -format "format profile"
+(eg.: "python aggregator.py -path "D:\YTDL\" -sub "en,ru,zh-Hans" -format "best[height<=480]")
+For more media profiles - check https://github.com/yt-dlp/yt-dlp#format-selection-examples 
 
 It will download all the newly uploaded videos for each channel, including subtitles and descriptions.
 Also it will create archive.txt file, and update it with the downloaded indexes, so that at the next run it will not re-download already downloaded files.
 
-Video demo here: https://youtu.be/DMfk8zgrsNI 
+
+B. Binary (easy) - make sure you add / update all your channels in the "data" folder
+Run from terminal: 
+
+Open bin/ folder
+
+aggregator.exe  -path "D:\YTDL\" -sub "en,ru,zh-Hans" -format "best[height<=480]"
+
+
+Video demo here: https://www.youtube.com/watch?v=gYtrAmYssA8 (a bit outdated)
--- aggregator.py
+++ aggregator.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os, os.path
 import subprocess
 from subprocess import Popen, PIPE, CalledProcessError
@@ -6,74 +8,188 @@ import fnmatch
 import argparse
 import datetime
 import sys
+import shutil
+import random
+import pdb 
+import glob
+import re
 
-### TOFIX:
-#WARNING: Unable to download webpage: <urlopen error [Errno 61] Connection refused> 
-#TO compile into EXE pyinstaller --onefile -w aggregator.py
-#ERROR: Unable to download webpage: <urlopen error [Errno 61] Connection refused> (caused by URLError(ConnectionRefusedError(61, 'Connection refused')))
+"""
+***********************
+******Debugging:*******
+***********************
+Use PDB for debugging purposes, just place the line anywhere:
+https://docs.python.org/3/library/functions.html
+pdb.set_trace()
+p dir(<obj>)
 
-# PRE_REQUISITES 
-# pip install -U yt-dlp; #SRC https://github.com/yt-dlp/yt-dlp
+To download comments (slow): --write-comments
+Debug1: yt-dlp --yes-playlist -v --newline --progress --restrict-filenames --write-auto-subs --write-subs --sub-langs  "en,ru,zh-Hans" -f "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b" --recode-video mp4 --download-archive archive.txt --write-description -ciw -a index.txt
+Debug1: yt-dlp --yes-playlist -v --newline --progress --restrict-filenames --embed-thumbnail --write-thumbnail --embed-metadata --write-auto-subs --write-subs --sub-langs  "en,ru,zh-Hans" -f "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b" --recode-video mp4 --part --write-playlist-metafiles --download-archive archive.txt --write-description -ciw -a index.txt 
 
-parser = argparse.ArgumentParser(description='Specify your base dirrectory where all Youtube channels are saved')
-parser.add_argument('string', help='Input dirrectory location', nargs='+')
-args = parser.parse_args()
-baseDir = ' '.join(args.string)
-dirList = os.listdir(baseDir)
-subtitleLanguages = 'en,ru,zh-Hans' # 'en,es,iw,ro,ru,zh-Hans'
-# Download the best video with h264 codec, or the best video if there is no such video; # For other examples: https://github.com/yt-dlp/yt-dlp#format-selection-examples 
-# mediaFormat = "(bv*[vcodec^=avc1]+ba) / (bv*+ba/b) " 
-mediaFormat = "best[height<=480]"
+WITH METADATA
+with Popen("yt-dlp --yes-playlist -v --newline --progress --restrict-filenames --embed-thumbnail --embed-metadata --write-thumbnail --write-auto-subs --write-subs --sub-langs " + subtitleLanguages + " -f " + '"' + mediaFormat + '"' + " --recode-video mp4 --part --write-playlist-metafiles --download-archive archive.txt --write-description -ciw "  \"something with spaces\"" + channelUrl
+"""
 
-# itterate through all the existing dirrectories and read index.txt files to get the base URL for each channel
-for x in range(len(dirList)):
-	if os.path.isdir(baseDir + dirList[x]):
-		targetDir = (baseDir + dirList[x])
-		numberOfMediaFilesInDirectoryBeforeDownload = len(fnmatch.filter(os.listdir(targetDir),'*.mp4'))
-		print("Starting downloading: " + dirList[x])
-		print("Existing files: " + str(numberOfMediaFilesInDirectoryBeforeDownload)) 
+
+#TODO: to be tested, after folder naming refactoring
+def offload(input, outut):
+	destination_dir = outut
+	for dir_name in glob.iglob(input + '*', recursive=True):
+		if os.path.isdir(dir_name):
+			for subdir_name in glob.iglob(dir_name + '*', recursive=True):
+				print("\n")
+				print("Moving: " + str(subdir_name))
+				for file_name in glob.iglob(subdir_name + '/**/*.*', recursive=True):
+					channel_name_from_dir = subdir_name.split("\\")[2]
+					print("File: " + str(file_name))
+					if os.path.isfile(os.path.join(destination_dir + channel_name_from_dir + "\\", file_name)):
+						shutil.copy(file_name,  destination_dir + channel_name_from_dir + "\\") # , dirs_exist_ok=True
+						os.remove(file_name)
+					else: 
+						shutil.move(file_name,  destination_dir + channel_name_from_dir + "\\")
+
+#TODO: to be tested, after folder naming refactoring
+def generate_missing_dirs_for_present_archives():
+	print("\n")
+	# for file_name in glob.iglob('Desktop/**/*.txt', recursive=True):
+	for file_name in glob.iglob(baseDir + '*.txt', recursive=True):
+		pattern_for_Id = r"\((.*?)\)"
+		stringId = re.search(pattern_for_Id, file_name)
+		channelUrlId = stringId.group(1)
+		channelShortName = file_name.split("\\")[2].split("(")[0][:-1]
+		channel_dir_name = baseDir + channelShortName + " (" + channelUrlId + ")" 
+		#TODO - check others
 		
-		timeStamp = datetime.datetime.now()
-		channelUrl = ""
+		for dir_name in glob.iglob(baseDir + '*', recursive=True):
+			if os.path.isdir(dir_name):
+				for subdir_name in glob.iglob(dir_name + '*', recursive=True):
+					if not os.path.exists(channel_dir_name):
+						os.makedirs(channel_dir_name)
 
-		if (targetDir.__contains__("LgcyChn")):
-			channelUrl = "https://www.youtube.com/channel/"+ str(targetDir.split(" ")[1]) + "/"
-		else:
-			channelUrl = "https://www.youtube.com/" + str(targetDir.split(" ")[1]) + "/videos/" 
+def dir_path(path):
+    if os.path.isdir(path):
+        return path
+    else:
+        raise argparse.ArgumentTypeError(f"readable_dir:{path} is not a valid path")
 
-		reportFile=open(targetDir + '/' + 'yt_downloads_aggregator_report_' + timeStamp.strftime("%Y_%m_%d-%H_%M_%S") + '.txt', 'a+', encoding='utf-8')
-		reportFile.write(dirList[x] + ":")
-		reportFile.write(" " + str(len(fnmatch.filter(os.listdir(targetDir),'*.mp4'))) + " files before download")
-		reportFile.write('\n')
 
-		# Popen Docs https://docs.python.org/2/library/subprocess.html#subprocess.Popen 
-		# To download comments (slow): --write-comments
-		with Popen("yt-dlp --yes-playlist -v --newline --progress --restrict-filenames --write-auto-subs --write-subs --sub-langs " + subtitleLanguages + 
-			" -f " + '"' + mediaFormat + '"' + " --recode-video mp4 --download-archive archive.txt --write-description -ciw " + channelUrl,
-			stdout=PIPE, cwd=targetDir, bufsize=1, universal_newlines=True) as ytDlpProcess:
-			for line in ytDlpProcess.stdout:
-				print(line, end='') # process line here
-				standardOutput = ytDlpProcess.stdout.read()
-				# std_err = ytDlpProcess.stderr.read()
+def aggregate(argv):
 
-		if ytDlpProcess.returncode != 0:
-			raise CalledProcessError(ytDlpProcess.returncode, ytDlpProcess.args)
+	parser = argparse.ArgumentParser(description='Specify your base dirrectory where all Youtube channels will be saved')
+	parser.add_argument('-path', help='Input dirrectory location',type=dir_path)
+	parser.add_argument('-sub', help='Subtitles to be downloaded', nargs='?')
+	parser.add_argument('-format', help='Encoding format, for more details see https://github.com/yt-dlp/yt-dlp#format-selection-examples', nargs='?')
+	parser.add_argument('-offload', help='Run Offload utility, will move all your downloaded videos to a specified location (could be an external HDD)', nargs='+')
+	args = parser.parse_args()
 
-		reportFile.write('\n')
-		reportFile.write('Youtube-Dl OUTPUT: ')
-		reportFile.write(standardOutput)
-		reportFile.write('\n')
+	#If no args are provided, print help and exit
+	if len(sys.argv) < 2 and argv[0] not in '-offload': 
+	    print("Path: 1 - Not enough arguments")
+	    parser.print_help()
+	elif len(argv) >= 2 and argv[0] in '-offload':
+		print("Path: 2 - Offloading files")
+		pdb.set_trace()
+		inputDir = argv[1]
+		outputDir = argv[2]
+		# offload(inputDir, outputDir) #TO BE TESTED
+	elif argv[0] not in '-offload':
+		print("Path: 3 - Preparing to start the downloader")
+		baseDir = args.path
+		subtitleLanguages = args.sub 
+		mediaFormat = args.format 
+		dirList = os.listdir(baseDir)
 
-		# reportFile.write('Youtube-Dl ERRORS: ')
-		# reportFile.write(std_err)
-		# reportFile.write('\n')
-		
-		print("Finished downloading: " + dirList[x])
-		numberOfFilesAfterDownloadCompleted = len(fnmatch.filter(os.listdir(targetDir),'*.mp4'))
-		print("Total number of files after update: " + str(numberOfFilesAfterDownloadCompleted))
-		print("Files downloaded since the last update: " + str(numberOfFilesAfterDownloadCompleted - numberOfMediaFilesInDirectoryBeforeDownload)) 
-		print('\n')
-		reportFile.close()
+		# itterate through all the existing dirrectories and read 2nd argument to get the base URL for each channel
+		for x in range(len(dirList)):
+			if os.path.isdir(os.path.join(baseDir + dirList[x])) and len(dirList[x].split(" "))>1:
+				targetDir = (baseDir + dirList[x])
+				channelName = str(targetDir).split(" ")[0]
+				channelUrlId = re.sub(r"\(|\)", "", str(targetDir).split(" ")[1])
+				timeStamp = datetime.datetime.now()
+				channelTextArchive =  channelName + "_" + "(" + channelUrlId + ")" + ".txt"
+				numberOfMediaFilesInDirectoryBeforeDownload = len(fnmatch.filter(os.listdir(targetDir),'*.mp4'))
 
+				print("Path: 3a - Downloading channel: " + dirList[x])
+				print("Existing files: " + str(numberOfMediaFilesInDirectoryBeforeDownload)) 
+
+				pattern_for_Legacy_Id = r"[0-9A-Za-z_-]{23}[AQgw]$"
+				if (re.match(pattern_for_Legacy_Id, channelUrlId)):
+					channelUrl = "https://www.youtube.com/channel/" + channelUrlId + "/"
+				else: 
+					channelUrl = "https://www.youtube.com/" + channelUrlId + "/videos/"
+
+				command = ("yt-dlp --yes-playlist -v --newline --progress --restrict-filenames --no-hls-use-mpegts --embed-thumbnail" 
+				+ " --write-thumbnail --write-auto-subs --write-subs --sub-langs " + subtitleLanguages + " -f " + '"' + mediaFormat + '"' 
+				+ " --recode-video mp4 --part --write-playlist-metafiles --download-archive " + channelTextArchive 
+				+ " --write-description -ciw " + channelUrl)
+
+				reportFile=open(targetDir + '/' + 'yt_downloads_aggregator_report_' + timeStamp.strftime("%Y_%m_%d-%H_%M_%S") + '.txt', 'a+', encoding='utf-8')
+				reportFile.write(dirList[x] + ":")
+				reportFile.write(" " + str(len(fnmatch.filter(os.listdir(targetDir),'*.mp4'))) + " files before download")
+				reportFile.write('\n')
+
+				try:
+					# Popen Docs https://docs.python.org/2/library/subprocess.html#subprocess.Popen 
+					with Popen(command, shell=True, stdout=PIPE, cwd=targetDir, bufsize=1, universal_newlines=True) as ytDlpProcess:
+						for line in ytDlpProcess.stdout:
+							print(line, end='') # process line here
+							reportFile.write(line)
+							standardOutput = ytDlpProcess.stdout.read()
+							reportFile.write(standardOutput)
+							# std_err = ytDlpProcess.stderr.read()
+							# reportFile.write(std_err)
+							numberOfMediaFilesInDirectoryAfterDownload = len(fnmatch.filter(os.listdir(targetDir),'*.mp4'))
+
+						if ytDlpProcess.returncode != 0:
+							raise CalledProcessError(ytDlpProcess.returncode, ytDlpProcess.args)
+							#raise ExtractorError
+
+				except BaseException as e:
+						summary_report=open(baseDir + "/" + '_summary_report' + timeStamp.strftime("%Y_%m_%d") + '.txt', 'a+', encoding='utf-8')
+						summary_report.write('\n')
+						summary_report.write("Dowload dir: " + dirList[x])
+						summary_report.write('\n')
+						summary_report.write("Channel URL: " + channelUrl)
+						summary_report.write('\n')
+						summary_report.write("Existing files (before download): " + str(numberOfMediaFilesInDirectoryBeforeDownload)) 
+						summary_report.write('\n')
+						summary_report.write("Total number of files after update: " + str(numberOfMediaFilesInDirectoryAfterDownload))
+						summary_report.write('\n')
+						summary_report.write("Files downloaded since the last update: " + str(numberOfMediaFilesInDirectoryAfterDownload - numberOfMediaFilesInDirectoryBeforeDownload)) 
+						summary_report.write('\n')
+						summary_report.write("Debug Command: " + str(e.args))
+						summary_report.write('\n')
+						summary_report.write('\n')
+						pass 
+
+				finally:
+					numberOfFilesAfterDownloadCompleted = len(fnmatch.filter(os.listdir(targetDir),'*.mp4'))
+
+					reportFile.write('\n')
+					reportFile.write('Youtube-Dl OUTPUT: ')
+					reportFile.write(standardOutput)
+					reportFile.write('\n')
+
+					# reportFile.write('Youtube-Dl ERRORS: ')
+					# reportFile.write(std_err)
+					# reportFile.write('\n')
+					
+					print("Finished downloading: " + dirList[x])
+					print("Total number of files after update: " + str(numberOfFilesAfterDownloadCompleted))
+					print("Files downloaded since the last update: " + str(numberOfFilesAfterDownloadCompleted - numberOfMediaFilesInDirectoryBeforeDownload)) 
+					print('\n')
+					reportFile.close()
+			elif(os.path.isdir(os.path.join(baseDir + dirList[x])) and len(dirList[x].split(" "))<2): #TOFIX len(dirList)<1 )
+				print("Something went wrong. No valid youtube channels found. Check your folders structure. "
+					+ "Please create empty folders in the format: <RandomChosenName validYoutubeChanelName>")
+			else:
+				# print("Path: 3b - Skipping the files, only taking the dirrectories from the base_dir. Here be dragons...")
+				# print(baseDir + dirList[x] + " not a directory")
+				continue 
 	else:
-		print(baseDir + dirList[x] + " not a directory")
+		print("Path: 4 - Houston, we've got a problem...")
+		parser.print_help()
+
+if __name__ == '__main__':
+    aggregate(sys.argv[1:])
